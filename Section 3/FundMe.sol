61ad4186c22144294e348f4681d593b552eb776a
MrRobWeb <mister.robert.weber@gmail.com>
2025-01-22 07:52:05 +0100

    adding withdraw function; code refactoring

--- Section 3/FundMe.sol
+++ Section 3/FundMe.sol
@@ -8,16 +8,22 @@ contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH!");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH!");
         funders.push(msg.sender);
 
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
 
     }
 
@@ -27,10 +33,32 @@ contract FundMe {
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
-    // function withdraw() public {
-    
-    // }
+
+    function withdraw() public onlyOwner{
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) 
+        {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // resett the array 
+        funders = new address[](0);
+        // ***Send token to an address
+        // // transfer 
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send 
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call - recommended
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+        // ***Send token to an address
+    }
 
 
-    
+    modifier onlyOwner() {
+        require(msg.sender == i_owner, "Sender is not the owner!");
+        // whatever else is in the function
+        _;
+
+    }
 }
